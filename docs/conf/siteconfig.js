25e8355e2bc5fc39bdc10a8405e9ab36221223f9
zijianhuang <zijianhuang2000@gmail.com>
2023-09-05 11:01:14 +1000

    ok

---
+++ docs/conf/siteconfig.js
@@ -0,0 +1,14 @@
+const SITE_CONFIG = {
+	siteName: 'Poetry Collection of Poets',
+	staticReadonly: true,
+	copyPoemEnabled: true,
+	copyAlbumEnabled: false,
+	formFieldAppearance: 0,
+	staticDatas: [
+		{filename: 'Shelley', poetName: 'Percy Bysshe Shelley'},
+		{filename: 'Byron', poetName: 'George Gordon Byron'},
+		{filename: 'EdgarAllanPoe', poetName: 'Edgar Allan Poe'},
+		{filename: 'LiBai', poetName: '李白', secondaryFilename: 'LiBai2nd'}
+	]
+	
+}
---
+++ startProd.bat
@@ -0,0 +1,4 @@
+::Build for testing with dotnet hosting
+::set %current%=%~dp0
+
+dotnet-serve -d C:\VsProjects\Fonlow\poets\poets_gp -p 5300
