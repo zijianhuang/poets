cd648971bf89c6ee71d6d874b9de647658335906
Z <zijianhuang2000@gmail.com>
2026-02-16 08:55:06 +1000

    ok

--- docs/conf/siteconfig.js
+++ docs/conf/siteconfig.js
@@ -10,6 +10,12 @@ const SITE_CONFIG = {
 		"assets/themes/rose-red.css":{display: "Roes & Red", dark:false},
 		"assets/themes/magenta-violet.css":{display: "Magenta & Violet", dark:true},
 		"assets/themes/cyan-orange.css":{display: "Cyan & Orange", dark:true}
-	}
+	},
+	staticDatas: [
+		{filename: 'Shelley', poetName: 'Percy Bysshe Shelley'},
+		{filename: 'Byron', poetName: 'George Gordon Byron'},
+		{filename: 'EdgarAllanPoe', poetName: 'Edgar Allan Poe'},
+		{filename: 'LiBai', poetName: '李白', secondaryFilename: 'LiBai2nd'}
+	]
 }
 
