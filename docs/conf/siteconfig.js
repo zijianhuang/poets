2f81516d6f50075ece6a39ffc8759980105745c7
Z <zijianhuang2000@gmail.com>
2026-02-15 19:13:42 +1000

    ok

--- docs/conf/siteconfig.js
+++ docs/conf/siteconfig.js
@@ -1,14 +1,13 @@
 const SITE_CONFIG = {
 	siteName: 'Poetry Collection',
 	apiBaseUri: '',
-	themeName: 'azure-blue',
 	staticReadonly: true,
 	copyPoemEnabled: true,
 	copyAlbumEnabled: false,
 	chronologyEnabled: true,
 	themesDic: {
-		"assets/themes/rose-red.css":{display: "Roes & Red", dark:false},
 		"assets/themes/azure-blue.css":{display: "Azure & Blue", dark:false},
+		"assets/themes/rose-red.css":{display: "Roes & Red", dark:false},
 		"assets/themes/magenta-violet.css":{display: "Magenta & Violet", dark:true},
 		"assets/themes/cyan-orange.css":{display: "Cyan & Orange", dark:true}
 	}
